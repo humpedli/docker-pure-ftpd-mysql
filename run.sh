965f8a09f9e0f98d6c91bb6fae4456faae1ba9c8
humpedli <hump@hump.hu>
2018-06-26 15:20:13 +0200

    TLS fix

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ EOM
 TLS=0
 if [ -e /etc/ssl/private/imported-key.pem ] && [ -e /etc/ssl/private/imported-cert.pem ];
 then
-    cat /etc/ssl/private/imported-key.pem /etc/ssl/private/imported-cert.pem > /etc/ssl/private/pure-ftpd.pem
+    awk 1 /etc/ssl/private/imported-key.pem /etc/ssl/private/imported-cert.pem > /etc/ssl/private/pure-ftpd.pem
     chmod 600 /etc/ssl/private/pure-ftpd.pem
     TLS=1
 fi
@@ -48,4 +48,4 @@ fi
 # -A chroot everyone
 # -B daemonize
 # -P external ip for passive mode
-/usr/sbin/pure-ftpd-mysql -l mysql:/etc/pure-ftpd/db/mysql.conf -J 'ALL:!aNULL:!SSLv3' -E -O clf:/var/log/pure-ftpd/transfer.log -8 UTF-8 -u 30 -U 111:000 -p 30000:30009 -Y $TLS -H -A -B -P ${EXTERNAL_IP:-localhost} && tail -f && tail -f /var/log/*.log
+/usr/sbin/pure-ftpd-mysql -l mysql:/etc/pure-ftpd/db/mysql.conf -E -O clf:/var/log/pure-ftpd/transfer.log -8 UTF-8 -u 30 -U 111:000 -p 30000:30009 -Y $TLS -H -A -B -P ${EXTERNAL_IP:-localhost} && tail -f /var/log/*.log
